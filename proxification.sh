7c291521a5fde7ac497ec398620f2c7af6d1a378
iamtrazy <iamtrazy@protonmail.com>
2021-06-22 17:10:50 +0530

    Update proxification.sh

--- proxification.sh
+++ proxification.sh
@@ -9,6 +9,8 @@ ip link set tun0 up
 ip -6 link set tun0 up
 ip route add default dev tun0
 ip -6 route add default dev tun0
-sudo badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:7300 --netif-ip6addr fdfe:dcba:9876::2
+dns2socks 127.0.0.1:1080 1.1.1.1:53 127.0.0.1:53 > /dev/null & sleep 5 &&  sed -i '1s/^/nameserver 127.0.0.1 /' /etc/resolv.conf && badvpn-tun2socks --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:7300
+killall dns2socks
+sed -i 's/nameserver 127.0.0.1 //'  /etc/resolv.conf
 ip tuntap del dev tun0 mode tun
 ip route del $ssh_ip via $def_gate
